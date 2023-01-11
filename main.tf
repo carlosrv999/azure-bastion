94473ea5d57197c4c2b3a98a7a1b59065f291b06
Carlos Ramirez <carlosrv125@icloud.com>
2023-01-11 18:39:20 -0500

    added bastion

--- main.tf
+++ main.tf
@@ -8,9 +8,109 @@ resource "azurerm_virtual_network" "default" {
   location            = "East US"
   resource_group_name = azurerm_resource_group.default.name
   address_space       = ["10.0.0.0/16"]
+}
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.0.0/24"
+resource "azurerm_subnet" "default" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.0.1.0/26"]
+}
+
+resource "azurerm_network_interface" "default" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    //public_ip_address_id          = azurerm_public_ip.default.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "default" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  size                = "Standard_D2s_v5"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.default.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_security_group" "default" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = azurerm_subnet.bastion.address_prefixes[0]
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "default" {
+  network_interface_id      = azurerm_network_interface.default.id
+  network_security_group_id = azurerm_network_security_group.default.id
+}
+
+resource "azurerm_public_ip" "default" {
+  name                = "example-network-ip"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  sku                 = "Standard"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "bastion-public-ip"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_bastion_host" "default" {
+  name                = "example-network-bastion"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  ip_configuration {
+    name                 = "IpConf"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.default.id
   }
 }
